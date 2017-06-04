ea30f286738f261b51c6de69eecbfe90f76316c4
Saul Duphraigne <comarkgh8@gmail.com>
2017-06-04 13:52:19 +0200

    part2 specs passed

--- lib/practice_intro.rb
+++ lib/practice_intro.rb
@@ -17,20 +17,40 @@ end
 # Part 2
 
 def hello(name)
-  'Hello ' + name
+  'Hello, ' + name
 end
 
 def starts_with_consonant? s
-  #change to lower case
+  # change to lower case
   first_letter = s.downcase[0]
-  if first_letter =~ /[aeiou]/
-    return false
+  # make sure is letter
+  if first_letter =~ /[A-Za-z]/
+    if first_letter =~ /[aeiou]/ 
+      return false
+    end
+    return true
   end
-  true
+  false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  # if s=0 return 0
+  if s =~ /^0{1,}$/
+    return true
+  end
+  # make sure is bin number
+  # remove any zeros from beginning
+  s_no_zeros = s.sub(/^[0]*/,"")
+  if s_no_zeros.to_i.to_s !=s_no_zeros 
+    return false
+  end
+  # first convert to integer base 2
+  the_integer = s.to_i(2)
+  # check if mult of 4
+  if the_integer%4 == 0
+    return true
+  end
+  false
 end
 
 # Part 3
@@ -39,4 +59,4 @@ class BookInStock
 # YOUR CODE HERE
 end
 
-puts starts_with_consonant?('Saul')
+puts binary_multiple_of_4?("s00100")
